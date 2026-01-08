bead66a4b7ae9f4fb6a8102cc66b2c7a76b2201d
ArvinDelavari <arvin7807@gmail.com>
2026-01-08 12:06:51 +0330

    Testing makefile and new automated simulation flow (SUCCESS)

--- ARTSim.py
+++ ARTSim.py
@@ -1,20 +1,13 @@
 # Author: Adam Corbier (@Ad2Am2)
 
 import time
-import nub_ctm as ctm
-
+from src import globalVar
+from src import nub_ctm as ctm
 # Use name of model file instead of example_model
-from example_model import model
-
-import globalVar
-
-import numpy as np
-
-
-
+from models.example_model import model
+from src.runSimulations import *
 
 def prepareModel(model):
-
 
     model = ctm.flatten_model(model)
     print("Making nodes...")
@@ -43,75 +36,16 @@ def prepareModel(model):
 
     globalVar.modelPrepared = True
 
-
-
 # NOTE: make sure to call prepareModel before running the simulation
-def runSteadyState(outputFile):
-
-    if not globalVar.modelPrepared:
-        raise Exception("Model not prepared. Please call prepareModel(model) before running the simulation")
-
-    nodes = globalVar.nodes
-    model = globalVar.model
-    
-    print("Solving steady state...")
-    stepStart = time.time()
-
-    ssTempVector = ctm.solve_steady_state()
-
-    ssTempVector = [x + 318.5 for x in ssTempVector]
-
-    stepEnd = time.time()
-    print("Step time: " + str(stepEnd-stepStart))
-    ctm.printInfo(ssTempVector, nodes, model, outputFile)
-    
-
-
-
-# NOTE: make sure to call prepareModel before running the simulation
-def runTransient(stepDefinition, outputFile):
-
-    if not globalVar.modelPrepared:
-        raise Exception("Model not prepared. Please call prepareModel(model) before running the simulation")
-
-
-    nodes = globalVar.nodes
-    model = globalVar.model
-    GMatrix = globalVar.GMatrix
-    CMatrix = globalVar.CMatrix
-
-
-    steps = len(stepDefinition)
-    globalVar.IVectorVector = ctm.populate_I_vector_vector_transient(nodes, model, steps)
-
-    # Make initTempVector
-    initTempVector = []
-    for i in range(len(nodes)):
-        initTempVector.append(0)
-    # NOTE: The above should NOT start at the ambient temperature. It should start at the initial temperature, where ambient is 0.
-    
-
-
-    ttemp = ctm.doBeuler(GMatrix, CMatrix, globalVar.IVectorVector, initTempVector, stepDefinition)
-
-    for i in range(len(ttemp)):
-        ttemp[i] = [x + 318.5 for x in ttemp[i]]
-
-
-    ctm.saveTransientInfo(ttemp, stepDefinition, nodes, model, outputFile)
-
-    
-
-
 
 simStart = time.time()
 
 # Mandatory run before any simulation
 prepareModel(model)
 
-# Runs steady state analysis on the model, result in given output file
-runSteadyState("steadystate.txt")
+# Runs steady state analysis on the model, result in given logs file
 
+runSteadyState("logs/example_model_steadystate.log")
 
 # Transient simulation
 
@@ -121,8 +55,7 @@ runSteadyState("steadystate.txt")
 # Example stepDefinition:
 stepDefinition = [{"duration": 0.001, "steps": 2}, {"duration": 0.009, "steps": 2}, {"duration": 0.09, "steps": 2}, {"duration": 0.9, "steps": 2}, {"duration": 1, "steps": 2}]
 
-runTransient(stepDefinition, "transientResult.txt")
-
+runTransient(stepDefinition, "logs/example_model_transientResult.log")
 
 
 simEnd = time.time()
